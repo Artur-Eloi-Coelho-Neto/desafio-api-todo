ee9e3c1f4dc817be6dd4d4c35a365c92801dc41a
Artur-Eloi-Coelho-Neto <artureloiwork@gmail.com>
2026-08-31 16:44:44 -0300

    controller

---
+++ desafio-todolist/src/main/java/br/com/artureloi/desafio_todolist/controller/TodoController.java
@@ -0,0 +1,10 @@
+package br.com.artureloi.desafio_todolist.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    
+}
