2cc3a6fae212b76dfbeefb6e61778c6da07d5635
Artur-Eloi-Coelho-Neto <artureloiwork@gmail.com>
2026-08-31 16:13:21 -0300

    Service

---
+++ desafio-todolist/src/main/java/br/com/artureloi/desafio_todolist/service/TodoService.java
@@ -0,0 +1,32 @@
+package br.com.artureloi.desafio_todolist.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.artureloi.desafio_todolist.entity.Todo;
+
+@Service
+public class TodoService {
+    public List<Todo> create(){
+        return null;
+
+        
+    }public List<Todo> list(){
+        return null;
+
+        
+    }public List<Todo> update(){
+        return null;
+
+
+    
+    }public List<Todo> delete(){
+        return null;
+
+
+
+
+        
+    }
+}
